ef6af1897e49114caa260354e037de792d78dccf
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-06-28 16:10:34 -0700

    almost done with function stack, all but one test passed

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,31 @@
 var Stack = function() {
   var someInstance = {};
+  var size = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    if(size > 0) {
+      size--;
+    var popped = storage.size;
+    delete storage.size;
+    return popped;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
 };
+
+
