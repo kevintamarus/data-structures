ef4f15d1f273b83896434fe35c19424468614997
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-06-28 22:52:43 -0700

    finished function shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,51 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var modify = {
+    storage : {},
+    sizer : 0,
+    firstKey : 0,
+    lastKey : 0
+  };
+  _.extend(modify, queueMethods);
+  return modify;
 };
 
-var queueMethods = {};
+var queueMethods = {
 
-
+  enqueue : function(value) {
+    if(this.sizer === 0) {
+      this.sizer++
+      this.firstKey++;
+      this.lastKey++;
+      this.storage[this.firstKey] = value;
+    }
+    else {
+      this.storage[this.lastKey+1] = value;
+      this.sizer++;
+      this.lastKey++;
+    }
+  }
+  ,
+  dequeue : function() {
+    if (this.sizer > 1) {
+      this.sizer--
+      var dequeued = this.storage[this.firstKey];
+      delete this.storage[this.firstKey];
+      this.firstKey++
+      return dequeued;
+    }
+    else if (this.sizer === 1) {
+      this.sizer--
+      var dequeued = this.storage[this.lastKey];
+      delete this.storage[this.lastKey];
+      this.firstKey = 0;
+      this.lastKey = 0;
+      return dequeued;
+    }
+  }
+  ,
+  size : function() {
+    return this.sizer;
+  }
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var modify = {
+    storage : {},
+    sizer : 0
+  };
+  _.extend(modify, stackMethods);
+  return modify;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
-
+  push : function(value){
+    this.storage[this.sizer] = value;
+    this.sizer++;
+  }
+  ,
+  pop : function(){
+    if(this.sizer > 0) {
+      this.sizer--;
+      var popped = this.storage[this.sizer];
+      delete this.storage[this.sizer];
+    }
+    return popped;
+  }
+  ,
+  size : function(){
+    return this.sizer;
+  }
+};
