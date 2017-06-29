b41f6ced9f9152501c181d16d09583bb7fa93d6d
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-06-29 10:16:34 -0700

    part 1 finished

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,42 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.sizeOf = 0;
+  this.firstKey = 0;
+  this.lastKey = 0;
 };
 
+Queue.prototype.enqueue = function(value){
+  if(this.sizeOf === 0) {
+      this.sizeOf++
+      this.firstKey++;
+      this.lastKey++;
+      this.storage[this.firstKey] = value;
+    }
+    else {
+      this.storage[this.lastKey+1] = value;
+      this.sizeOf++;
+      this.lastKey++;
+    }
+};
 
+Queue.prototype.dequeue = function(){
+  if (this.sizeOf > 1) {
+    this.sizeOf--;
+    var dequeued = this.storage[this.firstKey];
+    delete this.storage[this.firstKey];
+    this.firstKey++;
+    return dequeued;
+  }
+  else if (this.sizeOf === 1) {
+    this.sizeOf--
+    var dequeued = this.storage[this.lastKey];
+    delete this.storage[this.lastKey];
+    this.firstKey = 0;
+    this.lastKey = 0;
+    return dequeued;
+  }
+};
+
+Queue.prototype.size = function(){
+  return this.sizeOf;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.sizeOf = 0;
+};
+
+Stack.prototype.push = function(value){
+  this.storage[this.sizeOf] = value;
+  this.sizeOf++;
+};
+
+Stack.prototype.pop = function(){
+  if (this.sizeOf > 0) {
+    this.sizeOf--;
+    var popped = this.storage[this.sizeOf];
+    delete this.storage[this.sizeOf];
+  }
+  return popped;
+};
+
+Stack.prototype.size = function(){
+  return this.sizeOf;
 };
 
 
