c8d21a30688dcdee557d03d5561f057c2042c505
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-06-30 16:18:52 -0700

    binary tree done

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -3,9 +3,11 @@ var BinarySearchTree = function(value) {
   binaryTree.value = value;
   binaryTree.left = null;
   binaryTree.right = null;
+  binaryTree.logArray = [];
   return binaryTree;
 };
 
+//complexity: O(log)
 BinarySearchTree.prototype.insert = function(value) {
   var isLeftEmpty = (this.left === null);
   var isRightEmpty = (this.right === null);
@@ -27,7 +29,36 @@ BinarySearchTree.prototype.insert = function(value) {
       this.right = BinarySearchTree(value);
     }
   }
+  this.logArray.push(value);
 };
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+
+//complexity: O(log)
+BinarySearchTree.prototype.contains = function(value) {
+  var isLeftEmpty = (this.left === null);
+  var isRightEmpty = (this.right === null);
+  //base case
+  if(value === this.value) {
+    return true;
+  } 
+  //edge cases
+  if (value < this.value && !isLeftEmpty) {
+    return this.left.contains(value);
+  }
+  if (value > this.value && !isRightEmpty) {
+    return this.right.contains(value);
+  }
+  if (value !== this.value && isLeftEmpty && isRightEmpty) {
+    return false;
+  }
+};
+
+//complexity: O(log)
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(cb);
+  }
+};
