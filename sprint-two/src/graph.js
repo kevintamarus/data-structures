0f3b2eb8c08164b4e46ad1dcc2041403ad12724c
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-06-30 21:21:50 -0700

    graph finished, complexity added, test added

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,17 +2,16 @@
 //if we want to connect the link, we add sets to the nested object for each key
 //if we want to remove the link, we need to remove all the links and also remove the current node
 
-// Instantiate a new graph
 var Graph = function() {
   this.storage = {};
 };
 
-// Add a node to the graph, passing in the node's value.
+//complexity: O(1)
 Graph.prototype.addNode = function(node) {
   this.storage[node] = {};
 };
 
-// Return a boolean value indicating if the value passed to contains is represented in the graph.
+//complexity: O(1)
 Graph.prototype.contains = function(node) {
   if (this.storage[node]) {
     return true;
@@ -20,7 +19,7 @@ Graph.prototype.contains = function(node) {
   return false;
 };
 
-// Removes a node from the graph.
+//complexity: O(n)
 Graph.prototype.removeNode = function(node) {
   for(var key in this.storage[node]) {
     delete this.storage[key][node];
@@ -28,7 +27,7 @@ Graph.prototype.removeNode = function(node) {
   delete this.storage[node];
 };
 
-// Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
+//complexity: O(1)
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   if(this.storage[fromNode][toNode]) {
   return true;
@@ -36,27 +35,22 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
   return false;
 };
 
-// Connects two nodes in a graph by adding an edge between them.
+//complexity: O(1)
 Graph.prototype.addEdge = function(fromNode, toNode) {
   this.storage[fromNode][toNode] = [toNode];
   this.storage[toNode][fromNode] = [fromNode]; 
 };
 
-// Remove an edge between any two specified (by value) nodes.
+//complexity: O(1)
 Graph.prototype.removeEdge = function(fromNode, toNode) {
   delete this.storage[fromNode][toNode];
   delete this.storage[toNode][fromNode];
 };
 
-// Pass in a callback which will be executed on each node of the graph.
+//complexity: O(n)
 Graph.prototype.forEachNode = function(cb) {
   for (var key in this.storage) {
     cb(key);
   }
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
 
