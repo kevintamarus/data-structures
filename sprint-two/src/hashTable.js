0a50e916f7435f69fdc6385830b6396869e7b7a4
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-07-01 12:00:58 -0700

    hash finished

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,6 +3,7 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
+//complexity 0(1)
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage.get(index) !== undefined && this._storage.get(index).length > 0) {
@@ -19,8 +20,23 @@ HashTable.prototype.insert = function(k, v) {
   }
 };
 
+//complexity 0(n)
 HashTable.prototype.retrieve = function(k) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) !== undefined) {
+    for (var i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === k) {
+        return this._storage.get(index)[i][1];
+      }
+    }
+  } 
+  else {
+    return undefined;
+  }
 };
 
+//complexity 0(1)
 HashTable.prototype.remove = function(k) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
